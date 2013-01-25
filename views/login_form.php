057772d4a9fd19670966691f7a76ee05738b5fbf
Ryan Brodkin <ryan@brodkinca.com>
2013-01-24 20:10:27 -0800

    Fixing bug in which a 403 error would be triggered by the auth page.

--- views/login_form.php
+++ views/login_form.php
@@ -2,6 +2,6 @@
   Professional Support by &nbsp;
   <img src="<?php echo plugins_url('img/logo_sm.png', dirname(__FILE__))?>" alt="BCA">
 </a>
-<a class="bca-login-toggle" href="?bca-auth">
+<a class="bca-login-toggle" href="?bca-auth=1">
   Support Agent Login
 </a>
